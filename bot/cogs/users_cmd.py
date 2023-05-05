ea95b7bf02e612cb382ef07dc938f8cb173fc423
Demid Trofimov <demid.trofimov.2017@mail.ru>
2023-05-05 20:15:09 +0300

    - добавлен конфиг - добавлены команды

--- bot/cogs/users_cmd.py
+++ bot/cogs/users_cmd.py
@@ -1,4 +1,9 @@
+import aiohttp
+import disnake
 from disnake.ext import commands
+
+from bot.configs.main_config import RapidAPI_Key
+from bot.utils.generate_line import lines
 
 
 class CMDUsers(commands.Cog):
@@ -14,9 +19,63 @@ class CMDUsers(commands.Cog):
         print(f'Bot {self.bot.user} is ready to work!')
 
     @commands.command()
-    async def hello(self, ctx):
-        """Команда /hello"""
-        await ctx.reply('привет!')
+    async def bot(self, ctx):
+        return await ctx.reply('бот на месте!')
+
+    @commands.command()
+    async def joined(self, ctx, member: disnake.Member = None):
+        """Говорит, когда участник присоединился"""
+        if member is None:
+            return await ctx.reply(f'Укажите пользователя')
+        if member.joined_at:
+            # formats the join time/date like "5 years ago"
+            date_str = disnake.utils.format_dt(member.joined_at, "R")
+            return await ctx.send(f"{member} присоединился {date_str}")
+
+    @commands.command()
+    async def gpt(self, inter: disnake.MessageInteraction, arg: str = None):
+        """ChatGPT"""
+        messages = await inter.message.reply("❇|Подождите, ваш запрос обрабатывается!")
+        try:
+            if arg is None:
+                return await messages.edit("📥| Введите параметры!")
+            url = "https://chatgpt-api7.p.rapidapi.com/ask"
+            payload = {
+                "query": f'{arg}',
+                "wordLimit": "4096",
+                "conversationId": "<Id from 1st response>"
+            }
+            headers = {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": RapidAPI_Key,
+                "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com"
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=payload, headers=headers) as response:
+                    await messages.edit(lines[0])
+                    for line in lines:
+                        await messages.edit(line)
+                    result = await response.json()
+                    return await messages.edit(result['response'])
+        except Exception as e:
+            return await messages.edit(f'Произошла ошибка на стороне ChatGPT!\n\n>{e}')
+
+    @commands.command()
+    async def commands(self, ctx):
+        embed = disnake.Embed(
+            title="Навигация по командам",
+            description="Здесь ты сможешь найти доступные команды и их описание"
+        )
+        commands_list = ["/gpt"]
+        descriptions_for_commands = ["ChatGPT"]
+        for command_name, description_command in zip(commands_list, descriptions_for_commands):
+            embed.add_field(
+                name=command_name,
+                value=description_command,
+                inline=False  # Будет выводиться в столбик, если True - в строчку
+            )
+        return await ctx.send(embed=embed)
+
 
 
 def setup(bot):
--- bot/configs/main_config.py
+++ bot/configs/main_config.py
@@ -1,3 +1,5 @@
 import os
 
-token = os.getenv('bot_token', '')
+token = os.getenv('bot_token', '')  # токен бота
+
+RapidAPI_Key = os.getenv('RapidAPI_Key', '')  # токен от api gpt
